1f4d0d64d5a1380700d83b797661756e0cd7f78f
bpadros <bpadros@gmail.com>
2022-07-27 00:17:28 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,32 @@
+const characterData = {
+    hero: {
+        name: "Liu Kang",
+        avatar: "images/liu kang.jpg",
+        health: 60,
+        diceCount: 3,
+        currentDiceScore: []
+    },
+    orc: {
+        name: "Reptile",
+        avatar: "images/reptile.jpg",
+        health: 30,
+        diceCount: 1,
+        currentDiceScore: []
+    },
+    demon: {
+        name: "Escorpion",
+        avatar: "images/escorpion.jpg",
+        health: 25,
+        diceCount: 2,
+        currentDiceScore: []
+    },
+    goblin: {
+        name: "Subzero",
+        avatar: "images/subzero.jpg",
+        health: 20,
+        diceCount: 3,
+        currentDiceScore: []
+    }
+}
+
+export default characterData
